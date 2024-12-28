4de222ee49c44b101b535579bb6c1d3b4d44febb
Trung Le Tan <letantrung372@gmail.com>
2024-12-28 20:03:38 +0700

    update pipeline

--- vars/microservicePipeline.groovy
+++ vars/microservicePipeline.groovy
@@ -2,9 +2,19 @@ import org.company.template.FrontendPipeline
 import org.company.template.BackendPipeline
 
 def call(Map config) {
+    // If no script parameter is provided, use 'this'
+    def scriptContext = config.script ?: this
+    
+    // Merge the provided config with defaults
+    def pipelineConfig = [
+        serviceType: config.type ?: 'backend',  // default to backend if not specified
+        serviceName: config.name ?: "${config.type}-service",
+        environment: config.environment ?: 'dev'
+    ]
+
     def pipeline
     
-    switch(config.serviceType) {
+    switch(pipelineConfig.serviceType) {
         case 'frontend':
             pipeline = new FrontendPipeline(this, config)
             break
