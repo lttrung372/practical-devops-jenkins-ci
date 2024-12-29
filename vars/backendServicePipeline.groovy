6801d20498f005c1a7c3a089aefbd04755f3ad75
Trung Le Tan <letantrung372@gmail.com>
2024-12-29 11:25:46 +0700

    update

--- vars/backendServicePipeline.groovy
+++ vars/backendServicePipeline.groovy
@@ -1,12 +1,13 @@
 #!/usr/bin/env groovy
+
 void call(Map pipelineParams) {
     def AWS_REGION = 'ap-southeast-1'
     def ECR_REPO = 'practical-devops-ecr'
     def AWS_ACCOUNTID = '307946653621'
-    def serviceName = "backend"
-    def ECR_REGISTRY = "${AWS_ACCOUNTID}.dkr.ecr.${AWS_REGION}.amazonaws.com/${ECR_REPO}"
+    def serviceName = 'backend'
+    def ECR_REGISTRY = "${AWS_ACCOUNTID}.dkr.ecr.${AWS_REGION}.amazonaws.com"
     def IMAGE_TAG = "SD1096-${serviceName}.${BUILD_NUMBER}-${new Date().format('yyyyMMddHHmmss')}"
-    
+
     pipeline {
         agent any
 
@@ -21,45 +22,46 @@ void call(Map pipelineParams) {
         }
 
         stages {
-            stage('Load Pipeline') {
+            stage('Login to AWS ECR') {
+                steps {
+                    script {
+                        withAWS(credentials: 'AWSCredentails', region: AWS_REGION) {
+                            sh """
+                                aws ecr get-login-password --region ${AWS_REGION} | docker login --username AWS --password-stdin ${ECR_REGISTRY}
+                            """
+                        }
+                    }
+                }
+            }
+
+            stage('Build Docker Image') {
                 steps {
                     script {
                         dir('src/backend') {
-                            //pipelineTemplate("backend")
-                            withAWS(credentials: 'AWSCredentails', region: AWS_REGION) {
-                                stage('Login to AWS ECR') {
-                                    script {
-                                        sh """
-                aws ecr get-login-password --region ${AWS_REGION} | docker login --username AWS --password-stdin ${ECR_REGISTRY}
-                """
-                                    }
-                                }
-
-                                stage('Build Docker Image') {
-                                    script {
-                                        sh """
-                docker build -t ${ECR_REPOSITORY}:${IMAGE_TAG} .
-                """
-                                    }
-                                }
-
-                                stage('Tag Docker Image') {
-                                    script {
-                                        sh """
-                docker tag ${ECR_REPOSITORY}:${IMAGE_TAG} ${ECR_REGISTRY}/${ECR_REPOSITORY}:${IMAGE_TAG}
-                """
-                                    }
-                                }
-
-                                stage('Push to ECR') {
-                                    script {
-                                        sh """
-                docker push ${ECR_REGISTRY}/${ECR_REPOSITORY}:${IMAGE_TAG}
-                """
-                                    }
-                                }
-                            }
+                            sh """
+                                docker build -t ${ECR_REPO}:${IMAGE_TAG} .
+                            """
                         }
+                    }
+                }
+            }
+
+            stage('Tag Docker Image') {
+                steps {
+                    script {
+                        sh """
+                            docker tag ${ECR_REPO}:${IMAGE_TAG} ${ECR_REGISTRY}/${ECR_REPO}:${IMAGE_TAG}
+                        """
+                    }
+                }
+            }
+
+            stage('Push to ECR') {
+                steps {
+                    script {
+                        sh """
+                            docker push ${ECR_REGISTRY}/${ECR_REPO}:${IMAGE_TAG}
+                        """
                     }
                 }
             }
--- vars/frontendServicePipeline.groovy
+++ vars/frontendServicePipeline.groovy
@@ -1,17 +1,18 @@
 #!/usr/bin/env groovy
+
 void call(Map pipelineParams) {
     def AWS_REGION = 'ap-southeast-1'
     def ECR_REPO = 'practical-devops-ecr'
     def AWS_ACCOUNTID = '307946653621'
     def serviceName = "frontend"
-    def ECR_REGISTRY = "${AWS_ACCOUNTID}.dkr.ecr.${AWS_REGION}.amazonaws.com/${ECR_REPO}"
+    def ECR_REGISTRY = "${AWS_ACCOUNTID}.dkr.ecr.${AWS_REGION}.amazonaws.com"
     def IMAGE_TAG = "SD1096-${serviceName}.${BUILD_NUMBER}-${new Date().format('yyyyMMddHHmmss')}"
 
     pipeline {
         agent any
 
         triggers {
-            githubPush()
+            githubPush() // Ensure this trigger is properly defined in Jenkins
         }
 
         options {
@@ -21,51 +22,55 @@ void call(Map pipelineParams) {
         }
 
         stages {
-            stage('Load Pipeline') {
+            stage('Login to AWS ECR') {
+                steps {
+                    script {
+                        withAWS(credentials: 'AWSCredentails', region: AWS_REGION) {
+                            sh """
+                                aws ecr get-login-password --region ${AWS_REGION} | docker login --username AWS --password-stdin ${ECR_REGISTRY}
+                            """
+                        }
+                    }
+                }
+            }
+
+            stage('Build Docker Image') {
                 steps {
                     script {
                         dir('src/frontend') {
-                            // pipelineTemplate("frontend")
-                            withAWS(credentials: 'AWSCredentails', region: AWS_REGION) {
-                                stage('Login to AWS ECR') {
-                                    script {
-                                        sh """
-                aws ecr get-login-password --region ${AWS_REGION} | docker login --username AWS --password-stdin ${ECR_REGISTRY}
-                """
-                                    }
-                                }
-
-                                stage('Build Docker Image') {
-                                    script {
-                                        sh """
-                docker build -t ${ECR_REPOSITORY}:${IMAGE_TAG} .
-                """
-                                    }
-                                }
-
-                                stage('Tag Docker Image') {
-                                    script {
-                                        sh """
-                docker tag ${ECR_REPOSITORY}:${IMAGE_TAG} ${ECR_REGISTRY}/${ECR_REPOSITORY}:${IMAGE_TAG}
-                """
-                                    }
-                                }
-
-                                stage('Push to ECR') {
-                                    script {
-                                        sh """
-                docker push ${ECR_REGISTRY}/${ECR_REPOSITORY}:${IMAGE_TAG}
-                """
-                                    }
-                                }
-                            }
+                            sh """
+                                docker build -t ${ECR_REPO}:${IMAGE_TAG} .
+                            """
                         }
+                    }
+                }
+            }
+
+            stage('Tag Docker Image') {
+                steps {
+                    script {
+                        sh """
+                            docker tag ${ECR_REPO}:${IMAGE_TAG} ${ECR_REGISTRY}/${ECR_REPO}:${IMAGE_TAG}
+                        """
+                    }
+                }
+            }
+
+            stage('Push to ECR') {
+                steps {
+                    script {
+                        sh """
+                            docker push ${ECR_REGISTRY}/${ECR_REPO}:${IMAGE_TAG}
+                        """
                     }
                 }
             }
         }
 
         post {
+            always {
+                echo "Pipeline completed."
+            }
             cleanup {
                 cleanWs()
             }
