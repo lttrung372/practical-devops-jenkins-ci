7b74acff6999e1597cb1289a53994b9ed2203fee
Trung Le Tan <letantrung372@gmail.com>
2024-12-29 10:59:11 +0700

    add credentials

--- src/org/practicaldevops/Global.groovy
+++ src/org/practicaldevops/Global.groovy
@@ -10,45 +10,64 @@ package org.practicaldevops
 def loginToECR(args) {
     def AWS_REGION = args.AWS_REGION
     def ECR_REGISTRY = args.ECR_REGISTRY
-    def AWS_PROFILE = "practical-devop-aws"
-    script {
+
+    withAWS(credentials: 'AWSCredentails', region: AWS_REGION) {
+        stage('Login to AWS ECR') {
+            script {
                 sh """
                 aws ecr get-login-password --region ${AWS_REGION} | docker login --username AWS --password-stdin ${ECR_REGISTRY}
                 """
+            }
+        }
     }
 }
 
 def buildDockerImages(args) {
     def ECR_REPOSITORY = args.ECR_REPOSITORY
     def IMAGE_TAG = args.IMAGE_TAG
+    def AWS_REGION = args.AWS_REGION
 
-    script {
+    withAWS(credentials: 'AWSCredentails', region: AWS_REGION) {
+        stage('Build Docker Images') {
+            script {
                 sh """
                 docker build -t ${ECR_REPOSITORY}:${IMAGE_TAG} .
                 """
             }
+        }
+    }
 }
 
 def tagDockerImages(args) {
     def ECR_REPOSITORY = args.ECR_REPOSITORY
     def IMAGE_TAG = args.IMAGE_TAG
+    def AWS_REGION = args.AWS_REGION
 
-    script {
+    withAWS(credentials: 'AWSCredentails', region: AWS_REGION) {
+        stage('Tag Docker Images') {
+            script {
                 sh """
                 docker tag ${ECR_REPOSITORY}:${IMAGE_TAG} ${ECR_REGISTRY}/${ECR_REPOSITORY}:${IMAGE_TAG}
                 """
             }
+        }
+    }
 }
 
 def pushDockerImages(args) {
     def ECR_REPOSITORY = args.ECR_REPOSITORY
     def IMAGE_TAG = args.IMAGE_TAG
+    def AWS_REGION = args.AWS_REGION
 
-    script {
+    withAWS(credentials: 'AWSCredentails', region: AWS_REGION) {
+        stage('Push Docker Images') {
+            script {
                 sh """
                 docker push ${ECR_REGISTRY}/${ECR_REPOSITORY}:${IMAGE_TAG}
                 """
             }
+        }
+    }
 }
 
 def deployToEKS(args) {
@@ -58,19 +77,24 @@ def deployToEKS(args) {
     def ECR_REPOSITORY = args.ECR_REPOSITORY
     def IMAGE_TAG = args.IMAGE_TAG
     def CONTAINER_NAME = args.CONTAINER_NAME
+    def AWS_REGION = args.AWS_REGION
 
-    script {
-        sh """
+    withAWS(credentials: 'AWSCredentails', region: AWS_REGION) {
+        stage('Push Docker Images') {
+            script {
+                sh """
             # Update kubeconfig
             aws eks update-kubeconfig --name ${CLUSTER_NAME}
-            
+
             # Deploy to EKS
             kubectl set image deployment/${DEPLOYMENT_NAME} \
                 ${CONTAINER_NAME}=${ECR_REGISTRY}/${ECR_REPOSITORY}:${IMAGE_TAG} \
                 -n ${NAMESPACE}
-            
+
             # Wait for rollout to complete
             kubectl rollout status deployment/${DEPLOYMENT_NAME} -n ${NAMESPACE}
         """
+            }
+        }
     }
 }
