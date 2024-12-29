13920949d5a0219fcc7d6e007ced1147b00af9ff
Trung Le Tan <letantrung372@gmail.com>
2024-12-29 11:12:39 +0700

    test

--- vars/pipelineTemplate.groovy
+++ vars/pipelineTemplate.groovy
@@ -13,25 +13,59 @@ def call(serviceName) {
     def CONTAINER_NAME = "${serviceName}-container"
     def global = new Global()
 
+    // withAWS(credentials: 'AWSCredentails', region: AWS_REGION) {
+    //     stage('Login to AWS ECR') {
+    //         global.loginToECR(AWS_REGION: AWS_REGION, ECR_REGISTRY: ECR_REGISTRY)
+    //     }
+
+    //     stage('Build docker images') {
+    //         global.buildDockerImages(ECR_REPOSITORY: ECR_REPOSITORY, IMAGE_TAG:IMAGE_TAG)
+    //     }
+
+    //     stage('Tag docker images') {
+    //         global.tagDockerImages(ECR_REPOSITORY: ECR_REPOSITORY, IMAGE_TAG:IMAGE_TAG)
+    //     }
+
+    //     stage('Push docker images to ECR') {
+    //         global.pushDockerImages(ECR_REPOSITORY:ECR_REPOSITORY, IMAGE_TAG:IMAGE_TAG)
+    //     }
+
+    //     stage('Deploy to EKS') {
+    //         global.deployToEKS(CLUSTER_NAME:CLUSTER_NAME, NAMESPACE:NAMESPACE, DEPLOYMENT_NAME:DEPLOYMENT_NAME, ECR_REPOSITORY:ECR_REPOSITORY, IMAGE_TAG:IMAGE_TAG, CONTAINER_NAME:CONTAINER_NAME)
+    //     }
+    // }
     withAWS(credentials: 'AWSCredentails', region: AWS_REGION) {
+        
         stage('Login to AWS ECR') {
-            global.loginToECR(AWS_REGION: AWS_REGION, ECR_REGISTRY: ECR_REGISTRY)
+            script {
+                sh """
+                aws ecr get-login-password --region ${AWS_REGION} | docker login --username AWS --password-stdin ${ECR_REGISTRY}
+                """
+            }
         }
 
-        stage('Build docker images') {
-            global.buildDockerImages(ECR_REPOSITORY: ECR_REPOSITORY, IMAGE_TAG:IMAGE_TAG)
+        stage('Build Docker Image') {
+            script {
+                sh """
+                docker build -t ${ECR_REPOSITORY}:${IMAGE_TAG} .
+                """
+            }
         }
 
-        stage('Tag docker images') {
-            global.tagDockerImages(ECR_REPOSITORY: ECR_REPOSITORY, IMAGE_TAG:IMAGE_TAG)
+        stage('Tag Docker Image') {
+            script {
+                sh """
+                docker tag ${ECR_REPOSITORY}:${IMAGE_TAG} ${ECR_REGISTRY}/${ECR_REPOSITORY}:${IMAGE_TAG}
+                """
+            }
         }
 
-        stage('Push docker images to ECR') {
-            global.pushDockerImages(ECR_REPOSITORY:ECR_REPOSITORY, IMAGE_TAG:IMAGE_TAG)
-        }
-
-        stage('Deploy to EKS') {
-            global.deployToEKS(CLUSTER_NAME:CLUSTER_NAME, NAMESPACE:NAMESPACE, DEPLOYMENT_NAME:DEPLOYMENT_NAME, ECR_REPOSITORY:ECR_REPOSITORY, IMAGE_TAG:IMAGE_TAG, CONTAINER_NAME:CONTAINER_NAME)
+        stage('Push to ECR') {
+            script {
+                sh """
+                docker push ${ECR_REGISTRY}/${ECR_REPOSITORY}:${IMAGE_TAG}
+                """
+            }
         }
     }
 }
